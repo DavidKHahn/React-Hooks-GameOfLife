1fa924b23d0ff2b452f7c3d0bbdbaca3f9fdc8e1
David <dhahn123@gmail.com>
2019-12-14 23:58:27 -0800

    setup useState with grids

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+const numRows = 50;
+const numCols = 50;
+
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
+    for(let i = 0; i< numRows; i++) {
+      rows.push(Array.from(Array(numCols), () => 0));
+    }
+
+    return rows;
+  });
+
+  console.log(grid);
+
+  return <div>Hello World!</div>
 }
 
 export default App;
