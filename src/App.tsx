2e99330444d9832e060a7f033e351e3ab7d4e97b
David <dhahn123@gmail.com>
2019-12-15 00:08:00 -0800

    display grids

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,11 @@ const numCols = 50;
 
 const App: React.FC = () => {
   const [grid, setGrid] = useState(() => {
+
     const rows = [];
-    for(let i = 0; i< numRows; i++) {
+    for(let i = 0; i < numRows; i++) {
+      // initialize an Array of '0's
+      // 2nd param is a fn setting all values inside array to be 0
       rows.push(Array.from(Array(numCols), () => 0));
     }
 
@@ -16,7 +19,23 @@ const App: React.FC = () => {
 
   console.log(grid);
 
-  return <div>Hello World!</div>
+  return (
+    <div style={{
+      display: 'grid',
+      gridTemplateColumns: `repeat(${numCols}, 20px)`
+    }}
+    >
+    {grid.map((rows, i) =>
+    rows.map((col, k) =>
+    <div
+    key={`${i}-${k}`}
+    style={{ width: 20,
+      height: 20, backgroundColor: grid[i][k] ? 'pink' : undefined,
+      border: 'solid 1px black'
+    }} />)
+  )}
+  </div>
+  )
 }
 
 export default App;
